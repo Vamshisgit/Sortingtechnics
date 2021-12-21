80658d9d89a87bd71112e1fa42f4986bca666ece
Vamshisgit <96138104+Vamshisgit@users.noreply.github.com>
2021-12-21 16:37:44 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 int main()
-{	int i,j,n;
-	printf("enter the size of array value n=");
-	scanf("%d",&n);
-	int a[n],temp;
-	for(i=0;i<n;i++)
-	{
-	printf("enter array element a[%d]=",i);
-	scanf("%d",&a[i]);
-	}
+{	int i,j,n,temp;
+	int a[]={12,3,54,57,52,87,9,65,76,46,74,38,29};
+ 
 	for(i=0;i<n-1;i++)
 	{
 		for(j=0;j<n-i-1;j++)
